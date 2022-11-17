c2c4daa4b620af61681de54d5a5911fcc7f31b3e
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-18 01:03:51 +0900

    add: 項目名を表示した

--- pie-chart-sample/src/components/PieChartWithCustomizedLabel/PieChartWithCustomizedLabel.js
+++ pie-chart-sample/src/components/PieChartWithCustomizedLabel/PieChartWithCustomizedLabel.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { PieChart, Pie, Cell } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Line,
+  Legend,
+  Tooltip,
+  LabelList,
+} from "recharts";
 
 export const PieChartWithCustomizedLabel = () => {
   const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
+    { name: "散歩する", value: 400 },
+    { name: "買い物に行く", value: 300 },
+    { name: "映画を見る", value: 300 },
+    { name: "本を読む", value: 200 },
   ];
 
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
@@ -40,21 +48,34 @@ export const PieChartWithCustomizedLabel = () => {
   };
 
   return (
-    <PieChart width={400} height={400}>
-      <Pie
-        data={data}
-        cx={200}
-        cy={200}
-        labelLine={false}
-        label={renderCustomizedLabel}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChart>
+    <>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+          <LabelList
+            dataKey="name"
+            position="outside"
+            fill="gray"
+            stroke="gray"
+            offset={20}
+          />
+        </Pie>
+        <Legend verticalAlign="top" height={36} />
+        <Tooltip cursor={true} animationDuration={1000} />
+
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </PieChart>
+    </>
   );
 };
